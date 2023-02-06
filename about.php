4f7e19614d7ca6816b6b20205465f2bf6e933142
jappolack <japwahl@gmail.com>
2023-02-06 07:44:06 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright 2015 by Jerrick Hoang, Ivy Xing, Sam Roberts, James Cook, 
+ * Johnny Coster, Judy Yang, Jackson Moniaga, Oliver Radwan, 
+ * Maxwell Palmer, Nolan McNair, Taylor Talmage, and Allen Tucker. 
+ * This program is part of RMH Homebase, which is free software.  It comes with 
+ * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation
+ * (see <http://www.gnu.org/licenses/ for more information).
+ * 
+ */
+
+	session_start();
+	session_cache_expire(30);
+?>
+<html>
+	<head>
+		<title>
+			About
+		</title>
+		<link rel="stylesheet" href="styles.css" type="text/css" />
+	</head>
+	<body>
+		<div id="container">
+			<?PHP include('header.php');?>
+			<div id="content">
+				<strong><i>Angels on Wheels Charity Organization is a newly formed 501(c)(3) 
+					Non-Profit Organization dedicated to changing the lives
+					 of our neighbors who are at risk of becoming homeless and hungry.</i></strong>
+			</div>
+		<?PHP include('footer.inc');?>
+		</div>
+	</body>
+</html>
